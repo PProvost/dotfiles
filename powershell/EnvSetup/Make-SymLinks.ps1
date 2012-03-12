ff1ef0d696d5646e18d9b76b766cd9e42db713d8
PProvost <peter@provost.org>
2012-03-12 01:06:15 -0600

    Make-Symlinks.ps1 appears to work!

--- powershell/EnvSetup/Make-SymLinks.ps1
+++ powershell/EnvSetup/Make-SymLinks.ps1
@@ -1,3 +1,7 @@
+param(
+	[switch] $WhatIf=$false
+)
+
 #########################################################################
 # Check to make sure the environment is sane before doing anything
 #
@@ -16,11 +20,15 @@ function error-message([string] $message) {
 	write-host $message
 }
 
+trap [Exception] {
+	error-message "Unexpected error occurred. $_"
+	exit 1
+}
+
 write-host "---------------------------------------------------"
 write-host " Make-SymLinks.ps1 - Creates links and junctions"
 write-host " for my dotfiles setup."
 write-host "---------------------------------------------------"
-
 
 # Figure out what directory the script is in
 if (test-path variable:\hostinvocation) {
@@ -36,7 +44,7 @@ success-message "Make-Symlinks.ps1 running from $scriptLocation"
 
 # Figure out where the $scripts directory is
 # and confirm that the profile.ps1 script is there
-$sourcePowershellDirectory = split-path (split-path $scriptLocation)
+$sourcePowershellDirectory = split-path (split-path $scriptLocation) | resolve-path
 $sourceProfilePath = join-path $sourcePowershellDirectory "profile.ps1"
 if ((test-path $sourceProfilePath) -eq $false) {
 	error-message "profile.ps1 not found in $sourcePowershellDirectory. Aborting."
@@ -48,7 +56,7 @@ $sourceDotfilesDirectory = split-path $sourcePowershellDirectory
 success-message "Source dotfiles found at $sourceDotfilesDirectory"
 
 # Make sure the source .vimrc exists and is a file
-$sourceVimrcPath = join-path $sourceDotfilesDirectory ".vimrc"
+$sourceVimrcPath = join-path $sourceDotfilesDirectory ".vimrc" | resolve-path
 if ((test-path -pathType Leaf $sourceVimrcPath) -eq $false) {
 	error-message "Source $sourceVimrcPath does not exist or is not a file. Aborting"
 	exit 1
@@ -56,7 +64,7 @@ if ((test-path -pathType Leaf $sourceVimrcPath) -eq $false) {
 success-message "Source .vimrc found at $sourceVimrcPath"
 
 # Make sure the source .vim exists and is a directory
-$sourceVimDirectory = join-path $sourceDotfilesDirectory ".vim"
+$sourceVimDirectory = join-path $sourceDotfilesDirectory ".vim" | resolve-path
 if ((test-path -pathType Container $sourceVimDirectory) -eq $false) {
 	error-message "Source $sourceVimDirectory does not exist or is not a directory. Aborting."
 	exit 1
@@ -64,7 +72,7 @@ if ((test-path -pathType Container $sourceVimDirectory) -eq $false) {
 success-message "Source .vim directory found at $sourceVimDirectory"
 
 # Make sure the source .gitconfig exists and is a file
-$sourceGitconfigPath = join-path $sourceDotfilesDirectory ".gitconfig"
+$sourceGitconfigPath = join-path $sourceDotfilesDirectory ".gitconfig" | resolve-path
 if ((test-path -pathType Leaf $sourceGitconfigPath) -eq $false) {
 	error-message "Source $sourceGitconfigPath does not exist or is not a file. Aborting"
 	exit 1
@@ -72,7 +80,7 @@ if ((test-path -pathType Leaf $sourceGitconfigPath) -eq $false) {
 success-message "Source .gitconfig found at $sourceGitconfigPath"
 
 # Make sure the source .gitconfig exists and is a file
-$sourceGitignorePath = join-path $sourceDotfilesDirectory ".gitignore"
+$sourceGitignorePath = join-path $sourceDotfilesDirectory ".gitignore" | resolve-path
 if ((test-path -pathType Leaf $sourceGitignorePath) -eq $false) {
 	error-message "Source $sourceGitignorePath does not exist or is not a file. Aborting"
 	exit 1
@@ -81,9 +89,9 @@ success-message "Source .gitignore found at $sourceGitignorePath"
 
 # Check to see if the WindowsPowerShell profile directory already exists
 $targetProfileDirectory = split-path $profile
-if (test-path (split-path $profile)) {
+if (test-path $targetProfileDirectory) {
 	error-message "Directory $targetProfileDirectory already exists. Aborting."
-	exit 3
+	exit 1
 }
 success-message "Target PowerShell profile directory {$targetProfileDirectory} does not exist."
 
@@ -139,16 +147,37 @@ if ((get-module -name PSCX) -eq $null) {
 # Create all the links and whatnot
 
 write-host "Creating hard link from $targetVimrcPath to $sourceVimrcPath" -foregroundColor $informationColor
-write-host ">> new-hardlink $targetVimrcPath $sourceVimrcPath"
+if ($WhatIf -eq $true) {
+	write-host ">> new-hardlink $targetVimrcPath $sourceVimrcPath"
+} else {
+	new-hardlink $targetVimrcPath $sourceVimrcPath
+}
 
 write-host "Creating junction point from $targetVimfilesDirectory to $sourceVimDirectory" -foregroundColor $informationColor
-write-host ">> new-junction $targetVimfilesDirectory $sourceVimDirectory"
+if ($WhatIf -eq $true) {
+	write-host ">> new-junction $targetVimfilesDirectory $sourceVimDirectory"
+} else {
+	new-junction $targetVimfilesDirectory $sourceVimDirectory
+}
 
 write-host "Creating junction point from $targetProfileDirectory to $sourcePowershellDirectory"
-write-host ">> new-junction $targetProfileDirectory $sourcePowershellDirectory"
+if ($WhatIf -eq $true) {
+	write-host ">> new-junction $targetProfileDirectory $sourcePowershellDirectory"
+} else {
+	new-junction "$targetProfileDirectory" $sourcePowershellDirectory
+}
 
 write-host "Creating hard link from $targetGitconfigPath to $sourceGitconfigPath"
-write-host ">> new-hardlink $targetGitconfigPath $sourceGitconfigPath"
+if ($WhatIf -eq $true) {
+	write-host ">> new-hardlink $targetGitconfigPath $sourceGitconfigPath"
+} else {
+	new-hardlink $targetGitconfigPath $sourceGitconfigPath
+}
 
 write-host "Creating hard link from $targetGitignorePath to $sourceGitignorePath"
-write-host ">> new-hardlink $targetGitignorePath $sourceGitignorePath"
+if ($WhatIf -eq $true) {
+	write-host ">> new-hardlink $targetGitignorePath $sourceGitignorePath"
+} else {
+	new-hardlink $targetGitignorePath $sourceGitignorePath
+}
+
