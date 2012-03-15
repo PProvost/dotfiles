1f54fe5a73df219e4d7612912bb28180828f250b
PProvost <peter@provost.org>
2012-03-14 17:28:32 -0700

    Update to nuget profile

--- powershell/NuGet_profile.ps1
+++ powershell/NuGet_profile.ps1
@@ -9,6 +9,11 @@ $GitPromptSettings.BeforeForegroundColor = [ConsoleColor]::Gray
 $GitPromptSettings.AfterForegroundColor = [ConsoleColor]::Gray
 $GitPromptSettings.BranchForegroundColor = [ConsoleColor]::DarkYellow
 
+
+# Load up the VS Command prompt stuff
+$script:vsdir = $dte.FullName | split-path | split-path
+invoke-batchfile (join-path $vsdir "Tools\vsvars32.bat")
+
 function add-existingProject([string] $projFile) {
 	$dte.Solution.AddFromFile($projFile, $false)
 }
@@ -16,4 +21,15 @@ function add-existingProject([string] $projFile) {
 function open-file([string] $path) {
 	$dte.ItemOperations.OpenFile( $(resolve-path $path) ) | out-null
 }
-		
+
+function get-solutionDir {
+	$dte.Solution.FullName
+}
+
+function build-solution {
+	$dte.ExecuteCommand("Build.BuildSolution", "")
+}
+
+function clean-solution {
+	$dte.ExecuteCommand("Build.CleanSolution", "")
+}
